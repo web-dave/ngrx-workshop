06d7ec6aa983ef5570324b6192cc4fc5706d6dc5
Dave <dave@webdave.de>
2019-06-26 10:04:24 +0200

    changedetection onPush

--- src/app/book/book-list/book-list.component.ts
+++ src/app/book/book-list/book-list.component.ts
@@ -1,7 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef
+} from '@angular/core';
 import { Book } from '../shared/book';
 import { BookDataService } from '../shared/book-data.service';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { Store } from '@ngrx/store';
 import { booksStoreName, BooksState } from '../store/books.store';
 import { getBooks } from '../store/books.selectors';
@@ -9,18 +14,33 @@ import { getBooks } from '../store/books.selectors';
 @Component({
   selector: 'book-list',
   templateUrl: 'book-list.component.html',
-  styleUrls: ['book-list.component.css']
+  styleUrls: ['book-list.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class BookListComponent implements OnInit {
   books$: Observable<Book[]>;
+
   books: Book[];
+  bookSub = new BehaviorSubject(this.books);
+  i = 0;
 
   // constructor(private bookData: BookDataService) {}
-  constructor(private store: Store<BooksState>) {}
+  constructor(
+    private store: Store<BooksState>,
+    private cdr: ChangeDetectorRef
+  ) {}
 
   ngOnInit() {
     // this.books$ = this.bookData.getBooks();
     this.books$ = this.store.select(state => getBooks(state));
-    this.books$.subscribe(b => (this.books = b));
+    this.books$.subscribe(b => {
+      this.books = b;
+      // this.cdr.detectChanges();
+    });
+    setInterval(() => {
+      this.i++;
+      this.books[0].title = 'Design Patterns ' + this.i;
+      this.bookSub.next(this.books);
+    }, 1500);
   }
 }
--- src/app/book/book-preview/book-preview.component.ts
+++ src/app/book/book-preview/book-preview.component.ts
@@ -1,14 +1,35 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  OnChanges,
+  AfterContentChecked,
+  AfterViewChecked,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef
+} from '@angular/core';
 import { Book } from '../shared/book';
 
 @Component({
   selector: 'eis-book-preview',
   templateUrl: './book-preview.component.html',
-  styleUrls: ['./book-preview.component.scss']
+  styleUrls: ['./book-preview.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class BookPreviewComponent implements OnInit {
+export class BookPreviewComponent
+  implements OnInit, OnChanges, AfterViewChecked, AfterContentChecked {
   @Input() book: Book;
-  constructor() {}
+  constructor(private cdr: ChangeDetectorRef) {}
+  ngOnChanges(changes: import('@angular/core').SimpleChanges): void {
+    console.log(this.book.title, 'ngOnChanges', changes);
+  }
+  ngAfterViewChecked(): void {
+    console.log(this.book.title, 'ngAfterViewChecked');
+  }
+  ngAfterContentChecked(): void {
+    console.log(this.book.title, 'ngAfterContentChecked');
+    this.cdr.detectChanges();
+  }
 
   ngOnInit() {}
 }
