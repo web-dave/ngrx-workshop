2b58bed7778ed2fd28b6f98e4b85c488c28396d3
Dave <dave@webdave.de>
2019-07-06 11:16:44 +0200

    Select Data from Store

--- src/app/book/book-list/book-list.component.ts
+++ src/app/book/book-list/book-list.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Book } from '../shared/book';
 import { BookDataService } from '../shared/book-data.service';
 import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { booksStoreName, BooksState } from '../store/books.store';
+import { getBooks } from '../store/books.selectors';
 
 @Component({
   selector: 'book-list',
@@ -11,9 +14,10 @@ import { Observable } from 'rxjs';
 export class BookListComponent implements OnInit {
   books$: Observable<Book[]>;
 
-  constructor(private bookData: BookDataService) {}
+  constructor(private store: Store<BooksState>) {}
 
   ngOnInit() {
-    this.books$ = this.bookData.getBooks();
+    // this.books$ = this.bookData.getBooks();
+    this.books$ = this.store.select(getBooks);
   }
 }
