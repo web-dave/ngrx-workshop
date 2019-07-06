32d49f71552d38a0cbb687a499ae9400ea6b2f64
Dave <dave@webdave.de>
2019-07-06 09:52:14 +0200

    The first Reducer

--- src/app/book/store/books.reducer.ts
+++ src/app/book/store/books.reducer.ts
@@ -1,3 +1,21 @@
 // tslint:disable:max-line-length
 // import { Action, createSelector, createFeatureSelector } from '@ngrx/store';
 import { Book } from '../shared/book';
+import { initialState, BooksState } from './books.store';
+import { BookActions, LOAD_BOOKS } from './books.actions';
+
+export function booksReducer(
+  state = initialState,
+  action: BookActions
+): BooksState {
+  switch (action.type) {
+    case LOAD_BOOKS:
+      return {
+        ...state,
+        books: action.books
+      };
+    default: {
+      return state;
+    }
+  }
+}
