324f6700bead0bb00daf2c3e7fff7f20dc6984ad
Dave <dave@webdave.de>
2019-07-06 09:42:46 +0200

    Time for Action

--- src/app/book/store/books.actions.ts
+++ src/app/book/store/books.actions.ts
@@ -1,2 +1,17 @@
-// import { Action } from '@ngrx/store';
+import { Action } from '@ngrx/store';
 import { Book } from '../shared/book';
+
+export const LOAD_BOOKS = '[books] load books';
+export const WAIT_FOR_BOOKS = '[books] wait for books';
+
+export class LoadBooks implements Action {
+  readonly type: string = LOAD_BOOKS;
+  constructor(private books: Book[]) {}
+}
+
+export class WaitForBooks implements Action {
+  readonly type: string = WAIT_FOR_BOOKS;
+  constructor() {}
+}
+
+export type BookActions = LoadBooks | WaitForBooks;
