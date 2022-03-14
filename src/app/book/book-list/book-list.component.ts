275c2787223031f81acb6da90f21b04e33463ac4
web-dave <dave@webdave.de>
2022-03-14 23:46:39 +0100

    read books from store

--- src/app/book/book-list/book-list.component.ts
+++ src/app/book/book-list/book-list.component.ts
@@ -1,19 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { Book } from '../shared/book';
+import { IBook } from '../shared/book';
 import { BookDataService } from '../shared/book-data.service';
 import { Observable } from 'rxjs';
+import { getBooks } from '../store/books.selectors';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'book-list',
   templateUrl: 'book-list.component.html',
-  styleUrls: ['book-list.component.css']
+  styleUrls: ['book-list.component.css'],
 })
 export class BookListComponent implements OnInit {
-  books$: Observable<Book[]>;
+  books$: Observable<IBook[]>;
 
-  constructor(private bookData: BookDataService) {}
+  constructor(private store: Store) {}
 
   ngOnInit() {
-    this.books$ = this.bookData.getBooks();
+    this.books$ = this.store.select(getBooks);
   }
 }
