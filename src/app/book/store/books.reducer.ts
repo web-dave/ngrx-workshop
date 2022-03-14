7d9291ab353b9155160f65523f32038e524f277b
web-dave <dave@webdave.de>
2022-03-15 00:35:30 +0100

    refactor reducer to createReducer

--- src/app/book/store/books.reducer.ts
+++ src/app/book/store/books.reducer.ts
@@ -1,13 +1,10 @@
 // tslint:disable:max-line-length
-import { LOAD_BOOKS } from './books.actions';
+import { createReducer, on } from '@ngrx/store';
+import { LoadBooks, LOAD_BOOKS } from './books.actions';
 import { bookAdapter } from './books.entities';
 import { BooksState, initialState } from './books.store';
-export function booksReducer(state = initialState, action: any): BooksState {
-  switch (action.type) {
-    case LOAD_BOOKS:
-      return bookAdapter.setAll(action.books, state);
-    default: {
-      return state;
-    }
-  }
-}
+
+export const booksReducer = createReducer(
+  initialState,
+  on(LoadBooks, (state, { books }) => bookAdapter.setAll(books, state))
+);
