016bb400814579c8133f5d7e0010b5bcacca32ad
web-dave <dave@webdave.de>
2022-03-14 23:46:06 +0100

    reducer

--- src/app/book/store/books.reducer.ts
+++ src/app/book/store/books.reducer.ts
@@ -1,3 +1,20 @@
 // tslint:disable:max-line-length
 // import { Action, createSelector, createFeatureSelector } from '@ngrx/store';
-import { Book } from '../shared/book';
+import { IBook } from '../shared/book';
+import { BookActions, LoadBooks, LOAD_BOOKS } from './books.actions';
+import { BooksState, initialState } from './books.store';
+export function booksReducer(
+  state = initialState,
+  action: BookActions
+): BooksState {
+  switch (action.type) {
+    case LOAD_BOOKS:
+      return {
+        ...state,
+        books: (action as LoadBooks).books,
+      };
+    default: {
+      return state;
+    }
+  }
+}
