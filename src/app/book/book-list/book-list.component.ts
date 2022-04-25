48a41e63c6f8a81a489b979d1231696984a50c6d
web-dave <dave@webdave.de>
2022-04-25 11:22:00 +0200

    select

--- src/app/book/book-list/book-list.component.ts
+++ src/app/book/book-list/book-list.component.ts
@@ -2,18 +2,29 @@ import { Component, OnInit } from '@angular/core';
 import { Book } from '../shared/book';
 import { BookDataService } from '../shared/book-data.service';
 import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import {
+  BookCollectionSlice,
+  bookFeatureName,
+} from '../store/books-collection.slice';
 
 @Component({
   selector: 'book-list',
   templateUrl: 'book-list.component.html',
-  styleUrls: ['book-list.component.css']
+  styleUrls: ['book-list.component.css'],
 })
 export class BookListComponent implements OnInit {
-  books$: Observable<Book[]>;
+  books$: Observable<ReadonlyArray<Book>>;
 
-  constructor(private bookData: BookDataService) {}
+  constructor(
+    private store: Store<{
+      [bookFeatureName]: { bookCollection: BookCollectionSlice };
+    }>
+  ) {}
 
   ngOnInit() {
-    this.books$ = this.bookData.getBooks();
+    this.books$ = this.store.select(
+      (state) => state[bookFeatureName].bookCollection.entities
+    );
   }
 }
