e60d4eac4868c138918e7cccfea485d0d16b2008
web-dave <dave@webdave.de>
2022-04-25 10:38:22 +0200

    books-collection Reducer

--- src/app/book/store/books-collection.reducer.ts
+++ src/app/book/store/books-collection.reducer.ts
@@ -1,6 +1,16 @@
-import { createReducer } from '@ngrx/store';
+import { createReducer, on } from '@ngrx/store';
+import { createBookStart } from './books-collection.actions';
 import { BookCollectionSlice } from './books-collection.slice';
 const initialBookCollectionState: BookCollectionSlice = {
   entities: [],
 };
-export const bookCollectionReducer = createReducer(initialBookCollectionState);
+export const bookCollectionReducer = createReducer(
+  initialBookCollectionState,
+  //   on(createBookStart, (state, action) => {
+  //     return { ...state, entities: [...state.entities, action.book] };
+  //   }),
+  on(createBookStart, (state, { book }) => ({
+    ...state,
+    entities: [...state.entities, book],
+  }))
+);
