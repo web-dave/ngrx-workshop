428db6761f37a6694ea337631286f4dd24547629
web-dave <dave@webdave.de>
2022-04-26 12:12:33 +0200

    forms

--- src/app/book/book-new/book-new.component.ts
+++ src/app/book/book-new/book-new.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { Store } from '@ngrx/store';
+import { of } from 'rxjs';
 import { Book } from '../shared/book';
 import { BookDataService } from '../shared/book-data.service';
 import { createBookStart } from '../store/books-collection.actions';
@@ -12,26 +19,76 @@ import { createBookStart } from '../store/books-collection.actions';
 })
 export class BookNewComponent implements OnInit {
   form: FormGroup;
+  ctrlNames: string[] = [];
+
+  user = {
+    name: '',
+    adress: [
+      {
+        street: '',
+        city: 0,
+      },
+    ],
+  };
+  fa = new FormArray([]);
+  nameCtrl = new FormControl('', [], []);
 
   constructor(
-    private fb: FormBuilder,
+    private builder: FormBuilder,
     private bookService: BookDataService,
     private store: Store
   ) {}
 
   ngOnInit() {
-    this.form = this.fb.group({
-      isbn: [
-        '',
-        Validators.compose([
-          Validators.required,
-          Validators.minLength(13),
-          Validators.maxLength(13),
-        ]),
-      ],
-      title: ['', Validators.required],
-      author: ['', Validators.required],
+    of({
+      name: {
+        type: 'text',
+        required: true,
+      },
+      adress: {
+        type: 'text',
+        required: true,
+      },
+      age: {
+        type: 'number',
+      },
+    }).subscribe((data) => {
+      this.ctrlNames = Object.keys(data);
+      this.form = new FormGroup({});
+      this.ctrlNames.forEach((name) => {
+        this.form.addControl(
+          name,
+          new FormControl('', data[name].required ? [Validators.required] : [])
+        );
+      });
     });
+    // this.form = this.builder.group({
+    //   isbn: [
+    //     '',
+    //     [
+    //       Validators.required,
+    //       Validators.minLength(13),
+    //       Validators.maxLength(13),
+    //     ],
+    //     [],
+    //   ],
+    //   title: ['', Validators.required],
+    //   author: ['', Validators.required],
+    // });
+    // this.form.addControl('name', this.nameCtrl);
+
+    // this.form.get('isbn').valueChanges.subscribe((data) => {
+    //   if (data.length >= 1) {
+    //     console.table(data);
+    //     this.nameCtrl.setValue(data);
+    //     this.nameCtrl.addValidators([Validators.minLength(3)]);
+    //     this.nameCtrl.updateValueAndValidity();
+    //   } else {
+    //     this.nameCtrl.setValue('');
+    //     this.nameCtrl.removeValidators([Validators.minLength(3)]);
+    //     this.nameCtrl.updateValueAndValidity();
+    //   }
+    // });
   }
 
   onSubmit() {
